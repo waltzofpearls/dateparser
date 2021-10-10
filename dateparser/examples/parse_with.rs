9ebc134829f1090d56f955e6c35d63a86a9ddd2e
Rollie Ma <rollie@rollie.dev>
2021-10-09 23:18:17 -0700

    version: bump to 0.1.6 (#18)

---
+++ dateparser/examples/parse_with.rs
@@ -0,0 +1,16 @@
+use chrono::{
+    naive::NaiveTime,
+    offset::{Local, Utc},
+};
+use dateparser::parse_with;
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let parsed_in_local = parse_with("2021-10-09", &Local, NaiveTime::from_hms(0, 0, 0))?;
+    println!("{:#?}", parsed_in_local);
+
+    let parsed_in_utc = parse_with("2021-10-09", &Utc, NaiveTime::from_hms(0, 0, 0))?;
+    println!("{:#?}", parsed_in_utc);
+
+    Ok(())
+}
